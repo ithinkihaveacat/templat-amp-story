5b9b5bcfbc2610c8c1fc8d3caa95a90281bbbde6
Michael Stillwell <mjs@beebo.org>
2019-01-14 00:42:46 +0000

    Updates

--- AmpStory.tsx
+++ AmpStory.tsx
@@ -1,32 +1,57 @@
 import { React } from "./jsx";
 
-export default ({ children }: { [k: string]: any }) => {
-  return <html amp="" lang="en">
-    <head>
-      <meta charset="utf-8"/>
-      <script async="" src="https://cdn.ampproject.org/v0.js"></script>
-      <script async="" custom-element="amp-story" src="https://cdn.ampproject.org/v0/amp-story-1.0.js"></script>
-      <title>Hello, Ken Burns!</title>
-      <link rel="canonical" href="https://ithinkihaveacat.github.io/hello-world-amp-story/"/>
-      <link href="https://fonts.googleapis.com/css?family=Asap:700" rel="stylesheet"/>
-      <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1"/>
-      <style amp-boilerplate="">body&#123;-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both&#125;@-webkit-keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;@-moz-keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;@-ms-keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;@-o-keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;@keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;</style><noscript><style amp-boilerplate="">body&#123;-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none&#125;</style></noscript>
-    </head>
-    <body>
-      <amp-story
-        standalone=""
-        title="Hello, Ken Burns"
-        publisher="Michael Stillwell"
-        publisher-logo-src="https://s.gravatar.com/avatar/3928085cafc1e496fb3d990a9959f233?s=150"
-        poster-portrait-src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Cantilever_bridge_human_model.jpg/627px-Cantilever_bridge_human_model.jpg">
-        <amp-story-page id="start">
-          <amp-story-grid-layer template="thirds">
-            <div grid-area="middle-third">
-              <h1>Ready?</h1>
-            </div>
-          </amp-story-grid-layer>
-        </amp-story-page>
-      </amp-story>
-    </body>
-  </html>
-}
+export default ({
+  title,
+  publisher,
+  publisher_logo_src,
+  poster_portrait_src,
+  children
+}: {
+  [k: string]: any;
+}) => {
+  return (
+    <html amp="" lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <script async="" src="https://cdn.ampproject.org/v0.js" />
+        <script
+          async=""
+          custom-element="amp-story"
+          src="https://cdn.ampproject.org/v0/amp-story-1.0.js"
+        />
+        <title>{title}</title>
+        <link
+          rel="canonical"
+          href="https://ithinkihaveacat.github.io/hello-world-amp-story/"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Concert+One"
+          rel="stylesheet"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width,minimum-scale=1,initial-scale=1"
+        />
+        {/* prettier-ignore */}
+        <style amp-boilerplate="">body&#123;-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both&#125;@-webkit-keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;@-moz-keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;@-ms-keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;@-o-keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;@keyframes -amp-start&#123;from&#123;visibility:hidden&#125;to&#123;visibility:visible&#125;&#125;</style>
+        {/* prettier-ignore */}
+        <noscript><style amp-boilerplate="">body&#123;-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none&#125;</style></noscript>
+        <style amp-custom="">
+          amp-story &#123; font-family: Go, "Concert One", sans-serif; &#125;
+          amp-story-page * &#123; color: white; text-align: center; &#125;
+        </style>
+      </head>
+      <body>
+        <amp-story
+          standalone=""
+          title={title}
+          publisher={publisher}
+          publisher-logo-src={publisher_logo_src}
+          poster-portrait-src={poster_portrait_src}
+        >
+          {children}
+        </amp-story>
+      </body>
+    </html>
+  );
+};
--- hello.tsx
+++ hello.tsx
@@ -1,42 +1,22 @@
 import AmpStory from "./AmpStory";
+import {Theme, AmpStoryPage} from "./AmpStoryPage";
 
 import { React } from "./jsx";
 
-const Welcome = ({ first, last }: { first: string; last: string }) => {
-  if (first === "Clem") {
-    return <h1>Why hello there {first} {last}</h1>;
-  } else {
-    return <h1>Hello {first} {last}</h1>;
-  }
-};
-
-const Sidebar = ({ children }: { [k: string]: string }) => {
-  return <div class="sidebar">{children}</div>;
-};
-
-const a = { first: "jjj", last: "kkk" };
-
-const App = ({ last }: { last: string }) => (
-  <div>
-    <Sidebar>
-      <ul>
-        <li><Welcome first="a" last="b"/></li>
-        <li><Welcome {...a}/></li>
-      </ul>
-    </Sidebar>
-    <Welcome first="Clem" last={last} />
-    <Welcome first="Clint" last={last} />
-    <Welcome first="Cline" last={last} />
-  </div>
-);
-
-const context = {
-  title: "Foo Bar",
-  metadata: {
-    author: "Clem",
-    date: "today",
-  },
+const metadata = {
+  title: "Hello, AMP Story!",
+  publisher: "Michael Stillwell",
+  publisher_logo_src:
+    "https://s.gravatar.com/avatar/3928085cafc1e496fb3d990a9959f233?s=150",
+  poster_portrait_src:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Cantilever_bridge_human_model.jpg/627px-Cantilever_bridge_human_model.jpg"
 };
 
 console.log(`<!doctype html>`);
-console.log(<AmpStory {...context}><App last="Citizen"/></AmpStory>);
+console.log(
+  <AmpStory {...metadata}>
+    <AmpStoryPage theme={Theme.London} background="jjjj">
+      <h1>HULLO!!!</h1>
+    </AmpStoryPage>
+  </AmpStory>
+);
